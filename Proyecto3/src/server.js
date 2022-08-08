52f70fbec316531128af628f8013f1ef4fe173d7
gabiruca <gcsosa@espol.edu.ec>
2022-08-08 01:37:14 -0500

    Cambios

--- Proyecto3/src/server.js
+++ Proyecto3/src/server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/proyecto3-front'));
+app.use(express.static('./dist/proyecto3/proyecto3-front'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/proyecto3-front/'}),
+    res.sendFile('index.html', {root: 'dist/proyecto3/proyecto3-front/'}),
 );
 
 // Start the app by listening on the default Heroku port
